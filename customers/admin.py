7027d69fa437d83b28d9835707bbc4080b8e3af5
Landver <good1ni@mail.ru>
2020-03-01 19:38:04 +0300

    config for django admin panel

--- customers/admin.py
+++ customers/admin.py
@@ -1,23 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext_lazy as _
-
 from .models import User, EnrolmentRequest
 
 
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions', 'assigned_to', 'assigned_to_group', 'role')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-
-
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User)
 admin.site.register(EnrolmentRequest)
